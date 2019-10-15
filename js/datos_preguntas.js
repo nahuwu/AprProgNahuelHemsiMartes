63d4e40f690e0ffc849dfc02722b643dc9bb5641
nahuwu <56607466+nahuwu@users.noreply.github.com>
2019-10-15 16:09:18 -0300

    Add files via upload

---
+++ js/datos_preguntas.js
@@ -0,0 +1,48 @@
+/*
+Archivo: datos_preguntas.js
+Configuración de las preguntas del juego.
+*/
+
+let preguntas = [
+  {
+    pregunta:'¿Por qué debemos acelerar suave y progresivamente durante el desarrollo de una curva?',
+    opciones:[
+      'Para que los vehículos precedentes no nos adelanten',
+      'Para ahorrar en el gasto de combustible',
+      'Para que las ruedas se adhieran mejor a la calzada'
+    ],
+    imagen_src:'img/preguntas/imagen-generica.jpg',
+    respuesta_correcta: 'Para que las ruedas se adhieran mejor a la calzada'
+  },
+  {
+    pregunta:'¿Qué vehículo tiene prioridad para adelantarse cuando varios circulan en fila?',
+    opciones:[
+      'El último de la fila',
+      'El que circula primero detrás del vehículo que se desea adelantar',
+      'El que lo intente primero'
+    ],
+    imagen_src:'img/preguntas/autos.jpg',
+    respuesta_correcta: 'El último de la fila'
+  },
+  {
+    pregunta:'¿Qué significa la siguiente señal de tránsito?',
+    opciones:[
+      'Curva peligrosa',
+      'Curva pronunciada',
+      'Sentido de circulación'
+    ],
+    imagen_src:'img/preguntas/signo1.jpg',
+    respuesta_correcta: 'Curva pronunciada'
+  },
+  {
+    pregunta:'¿Cuál es la señal de camino sinuoso?',
+    opciones:[
+      'Opción A',
+      'Opción B',
+      'Opción C'
+    ],
+    imagen_src:'img/preguntas/3-signos.jpg',
+    respuesta_correcta: 'Opción B'
+  }
+
+];
