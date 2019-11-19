6d6cf4a008540dcca43cd5060fd7b11db7806fb3
nahuwu <56607466+nahuwu@users.noreply.github.com>
2019-11-19 20:50:27 -0300

    Add files via upload

--- js/mi_codigo.js
+++ js/mi_codigo.js
@@ -1,6 +1,6 @@
 
-let indice_pregunta_actual;
-let total_puntos;
+let indice_pregunta_actual = 0;
+let total_puntos = 0;
 let resultadomsg="";
 const nombre_alumno="Nahuel";
 const maximo_preguntas_por_jugada=1;
@@ -25,9 +25,31 @@ function obtenerSiguientePregunta() {
 
 function mostrarPregunta(pregunta){
 	document.querySelector("#pregunta-numero").textContent = (indice_pregunta_actual+1);
-	document.querySelector("#pregunta-texto").textContent = pregunta.texto_pregunta; // preguntar
-	document.querySelector("#pregunta-imagen").src = pregunta.imagen_src
+	document.querySelector("#pregunta-texto").textContent = preguntas.texto_pregunta; // preguntar
+	document.querySelector("#pregunta-imagen").src = preguntas.imagen_src
+
+
+div_opciones = document.querySelectorAll("#opciones div");
+input_opciones = document.querySelectorAll("#opciones input");
+label_opciones = document.querySelectorAll("#opciones label");
+
+
+for (let i = 0; i < 3; i++) {
+    // quita las clases correcta y erronea
+    div_opciones[i].classList.remove('correcta');
+    div_opciones[i].classList.remove('erronea');
+
+    // deschequea el radio button
+    input_opciones[i].checked = false;
+
+    // en el value del <input> pone el texto de la opción
+    input_opciones[i].value = pregunta.opciones[i];
+
+    // dentro del <label> escribe el texto de la opción
+    label_opciones[i].textContent = pregunta.opciones[i];
+  }
 }
+
 
 
 console.log(resultadomsg+" "+resultadomsg.toUpperCase());
@@ -41,6 +63,7 @@ function iniciarJuego(){
 	document.querySelector("#header").classList.remove("d-none");
 	document.querySelector("#pantalla-juego").classList.remove("d-none");
 	mostrarPregunta(indice_pregunta_actual);
+	mostrarPregunta(preguntas[indice_pregunta_actual]);
 }
 	indice_pregunta_actual = 0;
 	total_puntos = 0;
@@ -51,4 +74,3 @@ botonJugar.addEventListener("click", iniciarJuego);
 // ●  Bonus: Vamos a mostrar tu nombre en la pantalla “Acerca de”.“vista-nombre”. Mostrá el valor de la constante “nombre_alumno” dentro de ese elemento.
 let html_nombre_alumno = document.querySelector("#vista-nombre");
 html_nombre_alumno = nombre_alumno;
-
